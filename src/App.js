caeaa03895ba7454a7d5a8bdf6afefa1bceedcd5
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-03 16:55:52 -0400

    understanding state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Marcus', age: 24 },
+      { name: 'Jack', age: 30 },
+      { name: 'John', age: 20 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <Person name="Marcus" age="24" />
-        <Person name="Jack" age="30" >My Hobbies: Racing</Person>
-        <Person name="John" age="20" />
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }

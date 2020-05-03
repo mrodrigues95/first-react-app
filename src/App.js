dc3362d9a9bbead7889f1b7f0e07af9641572cc5
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-03 15:11:01 -0400

    props and props children

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <Person />
+        <Person name="Marcus" age="24" />
+        <Person name="Jack" age="30" >My Hobbies: Racing</Person>
+        <Person name="John" age="20" />
       </div>
     );
   }

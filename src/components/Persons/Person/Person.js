b7b3e13adbd9fc9e4cc0457717f8ad71ac9e1a9d
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-05 16:16:20 -0400

    added high order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 
-import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
+// import classes from './Person.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        ,<p>{this.props.children}</p>,
         <input
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
   }
 }

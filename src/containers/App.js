22d6cb6b5df235829ba95bf4e6ce288bc946c058
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-05 14:41:00 -0400

    messing around with lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Marcus', age: 28 },
@@ -14,6 +19,15 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Component did mount');
+  }
 
   nameChangedHandler = (event, id) => {
     // Find person.
@@ -48,6 +62,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] Render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -63,6 +78,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
